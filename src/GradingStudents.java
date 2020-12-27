6328288bf51dc858f1cf1c6353ab99ec35687e74
JayeshVibhute009 <learn.jayesh.vibhute@gmail.com>
2020-12-27 13:51:19 +0530

    Adding GradingStudents.java

---
+++ src/GradingStudents.java
@@ -0,0 +1,53 @@
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Result {
+
+	/*
+	 * Complete the 'gradingStudents' function below.
+	 *
+	 * The function is expected to return an INTEGER_ARRAY. The function accepts
+	 * INTEGER_ARRAY grades as parameter.
+	 */
+
+	public static List<Integer> gradingStudents(List<Integer> grades) {
+		// Write your code here
+
+		return IntStream.range(0, grades.size()).mapToObj(i -> {
+			Integer num = grades.get(i);
+			return (num < 38 || num % 5 < 3) ? num : num + (5 - (num % 5));
+		}).collect(toList());
+	}
+}
+
+public class GradingStudents {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+		List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+			try {
+				return bufferedReader.readLine().replaceAll("\\s+$", "");
+			} catch (IOException ex) {
+				throw new RuntimeException(ex);
+			}
+		}).map(String::trim).map(Integer::parseInt).collect(toList());
+		
+		List<Integer> result = Result.gradingStudents(grades);
+
+		bufferedWriter.write(result.stream().map(Object::toString).collect(joining("\n")) + "\n");
+
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
